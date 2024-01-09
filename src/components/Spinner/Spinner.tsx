912f4aee38541b510160a604c5db78f12780a3c8
Igor Khripunov <igor.khripunov@veeam.com>
2024-01-09 16:29:32 +0400

    Add spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,15 @@
+import './spinner.scss'
+
+
+const Spinner = () => (
+    <div className="cities">
+      <div className="cities__places-container container">
+        <div className="spinner">
+          <div className="nb-spinner"></div>
+        </div>
+      </div>
+    </div>
+  )
+;
+
+export {Spinner};
