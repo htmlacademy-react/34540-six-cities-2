ac2428b927718186165a3644ff45f9b6cf8cdf26
Igor Khripunov <igor.khripunov@veeam.com>
2024-01-28 13:36:14 +0400

    Update PrivateRoute

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,15 +1,20 @@
 import {Navigate} from 'react-router-dom';
+import {Spinner} from '../Spinner/Spinner.tsx';
 import {AppRoute, AuthorizationStatus} from '../../const.ts';
 import {useAppSelector} from '../../hooks';
 import {getAuthorizationStatus} from '../../store/user-process/selectors.ts';
 
 
 type TPrivateRouteProps = {
-  children: JSX.Element;
+  children: Element;
 }
 
 const PrivateRoute = ({children}: TPrivateRouteProps) => {
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
+
+  if (authorizationStatus === AuthorizationStatus.Unknown) {
+    return <Spinner/>;
+  }
 
   return (
     authorizationStatus === AuthorizationStatus.Auth
@@ -17,5 +22,6 @@ const PrivateRoute = ({children}: TPrivateRouteProps) => {
       : <Navigate to={AppRoute.Login}/>
   );
 };
+
 
 export {PrivateRoute};
