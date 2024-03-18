fef3d418b286008098606a78419d56eb4580df57
Igor Khripunov <igor.khripunov@veeam.com>
2024-03-18 16:13:47 +0400

    Fix Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -32,28 +32,33 @@ const Header = () => {
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
-              {authorizationStatus === AuthorizationStatus.Auth && (
+              {authorizationStatus !== AuthorizationStatus.Auth && (
                 <li className="header__nav-item user">
-                  <Link
-                    className="header__nav-link header__nav-link--profile"
-                    to={AppRoute.Favorites}
-                  >
+                  <Link className="header__nav-link" to={AppRoute.Login} onClick={onLoginClick}>
                     <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      {user}
-                    </span>
-                    <span className="header__favorite-count">{favoriteOffers.length}</span>
+                    <span className="header__login">Sign in</span>
                   </Link>
-                </li>)}
-              <li className="header__nav-item">
-                <Link className="header__nav-link" to={AppRoute.Login} onClick={onLoginClick}>
-                  <span
-                    className="header__signout"
-                  >
-                    {authorizationStatus === AuthorizationStatus.Auth ? 'Log Out' : 'Login'}
-                  </span>
-                </Link>
-              </li>
+                </li>
+              )}
+              {authorizationStatus === AuthorizationStatus.Auth && (
+                <>
+                  <li className="header__nav-item user">
+                    <Link
+                      className="header__nav-link header__nav-link--profile"
+                      to={AppRoute.Favorites}
+                    >
+                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                      <span className="header__user-name user__name">{user}</span>
+                      <span className="header__favorite-count">{favoriteOffers.length}</span>
+                    </Link>
+                  </li>
+                  <li className="header__nav-item">
+                    <Link className="header__nav-link" to={AppRoute.Login} onClick={onLoginClick}>
+                      <span className="header__signout">Sign out</span>
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </nav>
         </div>
